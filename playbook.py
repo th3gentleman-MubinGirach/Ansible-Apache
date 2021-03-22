b58b9f59ebcafc92216724c502ad0599ea5074f5
th3gentleman <72601671+th3gentleman-MubinGirach@users.noreply.github.com>
2021-03-22 18:34:20 +0530

    Create playbook.py

---
+++ playbook.py
@@ -0,0 +1,42 @@
+import os 
+
+os.system("tput setaf 6")
+print("Welcome to Ansible+Apache magic")
+
+def further():
+	print("Great lets move ahead")
+	os.system("touch new.html")
+	html=input("What should be the content of the html file you would like to keep: ")
+	filehtml = open("new.html", "w") 
+	filehtml.writelines(html) 
+	filehtml.close()
+	os.system("ansible-playbook playbook.yml")
+
+def inventory():
+   print("Since you dont have an inventory i am creating an inventory for you at /root named as inventory.txt")
+   os.system("touch inventory.txt")
+   defaultentry="[hosts]"
+   fileinv = open("inventory.txt", "w") 
+   fileinv.writelines(defaultentry) 
+   fileinv.close()
+   print("Thats great you need to add the following path root/inventory.txt config file /etc/ansible/ansible.cfg in the inventory parameter ")
+   
+       
+	
+
+
+	
+
+
+useri=input("Is your inventory good to go?: ")
+if useri == "yes":
+   further()
+
+elif useri == "no":
+    inventory()
+    
+
+else:
+ print("PLEASE CHECK YOUR INPUT")
+    
+ 
